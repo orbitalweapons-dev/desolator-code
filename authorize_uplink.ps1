41cc26a509e41e54566ad010b00cf35d55bd049e
orbitalweapons-dev <87558802+orbitalweapons-dev@users.noreply.github.com>
2022-03-16 14:51:14 -0700

    Update authorize_uplink.ps1

--- authorize_uplink.ps1
+++ authorize_uplink.ps1
@@ -1,4 +1,4 @@
 $desolator_ip = Read-Host "[?] Please enter the IP of the Desolator: "
-$authorization_code = '4a2dbd905287e75a5d2b659d2546fbab79abb21689e50f59492612df59bff460'
+$authorization_code = 'REDACTED'
 curl "${desolator_ip}:5000/api/v3/authorize?code=${authorization_code}"
 Read-Host -Prompt “Press Enter to exit”
