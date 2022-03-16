b23b18b26a1e69af1afa0e7250cad13ef5e3a824
orbitalweapons-dev <87558802+orbitalweapons-dev@users.noreply.github.com>
2022-03-16 14:51:42 -0700

    redacted

--- enable_uplink.sh
+++ enable_uplink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo '[?] Please enter the IP of the Desolator: '
 read desolator_ip
-uplink_code='c2fa26d57cd69ed4cf86f1a9cf8f0232cc20b24387e3299aa267224722bd31ef'
+uplink_code='redacted'
 host=$(hostname -I)
 curl "${desolator_ip}:5000/api/v3/enable?code=${uplink_code}&host=${host}"
